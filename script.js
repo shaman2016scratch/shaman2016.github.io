3c32514c4932029feb8b3bc8d3ec09c4787b185f
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-09-22 12:41:15 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Привет разработчик!");
+test = "testing js";
+console.log(test);
+const url = new URL(window.location);
+const searchParams = url.searchParams;
+pdebug = searchParams.get('_ym_debug');
+function debug() {
+  document.getElementById("title").textContent = `YandexMetrica_debug_${pdebug}`;
+}
+lang_ym = searchParams.get('?_ym_status-check=103647270&_ym_lang');
+function ymlang() {
+  document.getElementById("title").textContent = `YandexMetrica_ymlang_${lang_ym}`;
+}
+if pdebug = '' {
+  debug()
+}
+if lang_ym = '' {
+  ymlang()
+}
