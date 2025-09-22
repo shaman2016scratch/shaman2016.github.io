c1dd17e4b510b087eec0f8b4ffc280bd951ae58b
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-09-22 13:00:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,3 @@ console.log(test);
 const url = new URL(window.location);
 const searchParams = url.searchParams;
 pdebug = searchParams.get('_ym_debug');
-function debug() {
-  document.getElementById("title").textContent = `YandexMetrica_debug_${pdebug}`;
-}
-lang_ym = searchParams.get('_ym_status-check');
-function ymlang() {
-  document.getElementById("title").textContent = `YandexMetrica_ymlang_${lang_ym}`;
-}
-if pdebug = '' {
-  console.log("ym_debug не обнаружен!");
-} else {
-  debug();
-}
-if lang_ym = '' {
-  console.log("ym_status-check");
-} else {
-  ymlang();
-}
