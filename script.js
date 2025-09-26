c4d63d6c19ece3658a1657127630cdafac46b1ad
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-09-26 11:24:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ test = "testing js";
 console.log(test);
 const url = new URL(window.location);
 const searchParams = url.searchParams;
+ym(103647270,'reachGoal','view_page');
+viewpage = ym(103647270,'reachGoal','view_page')
