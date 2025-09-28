2852bda1686c0c3eb52f56408adb5a3a20295693
ilkham9999 <gilkham9@ya.ru>
2025-09-28 11:50:34 +0300

    Update 1.0.js

--- jsplugins/Extension1/1.0.js
+++ jsplugins/Extension1/1.0.js
@@ -1,9 +1,29 @@
 function pimport(i) {
   importName = i
+  if importName == "math" {
+    imp1 = (true)
+  }
 }
 function print(t) {
   console.log(t)
 }
-function print(t) {
-  console.log(t)
+function m(z1,z2,z3) {
+  if imp1 == (true) {
+    if z2 == "+" {
+      preturn = z1 + z3
+      return preturn
+    }
+    if z2 == "-" {
+      preturn = z1 - z3
+      return preturn
+    }
+    if z2 == "*" {
+      preturn = z1 * z3
+      return preturn
+    }
+    if z2 == "/" {
+      preturn = z1 / z3
+      return preturn
+    }
+  }
 }
