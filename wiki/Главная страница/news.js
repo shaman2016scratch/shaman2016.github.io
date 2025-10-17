9bfa332d7208f81be2f9cd9ef8af9c55b4a4b438
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-17 19:13:48 +0300

    Update news.js

--- wiki/Главная страница/news.js
+++ wiki/Главная страница/news.js
@@ -1,1 +1,7 @@
-
+function innerHTML(p1, p2) {
+  document.getElementById(p1).innerHTML = p2
+}
+function TextContent(p1, p2) {
+  document.getElementById(p1).textContent = p2
+}
+let soderzh = '<p></p>'
