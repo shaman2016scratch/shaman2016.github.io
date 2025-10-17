61eca6a0365eeddf809f324c4147f1dee0c1c46d
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-17 19:47:16 +0300

    Create pages.js

---
+++ wiki/Главная страница/pages.js
@@ -0,0 +1,8 @@
+function innerHTML(p1, p2) {
+  document.getElementById(p1).innerHTML = p2
+}
+function TextContent(p1, p2) {
+  document.getElementById(p1).textContent = p2
+}
+let pages = ''
+innerHTML('pages', pages)
