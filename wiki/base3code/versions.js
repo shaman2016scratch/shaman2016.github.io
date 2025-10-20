8cd47c613da3d048bf0028a156f4b12cd379475b
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-20 17:03:30 +0300

    Update versions.js

--- wiki/base3code/versions.js
+++ wiki/base3code/versions.js
@@ -1,1 +1,7 @@
-
+function innerHTML(p1, p2) {
+  document.getElementById(p1).innerHTML = p2
+}
+function TextContent(p1, p2) {
+  document.getElementById(p1).textContent = p2
+}
+let soderzh = '<h2>Версии base3code</h2><p><a href=\"https://scratch.mit.edu/projects/1227549189/\">1.0 альфа • https://scratch.mit.edu/projects/1227549189/</a></p><p><a href=\"https://scratch.mit.edu/projects/1228347683/\">1.0 бета • https://scratch.mit.edu/projects/1228347683/</a></p>'
