d0798ce02ef0e3b8bd3d03e52d77cb1d4da26c66
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-17 15:41:58 +0300

    Update 2.0.js

--- extensions/extension6/2.0.js
+++ extensions/extension6/2.0.js
@@ -5,12 +5,12 @@
 (async function(Scratch) {
 
     if (!Scratch.extensions.unsandboxed) {
-        alert("This extension needs to be unsandboxed to run!")
-        return
+      alert("This extension needs to be unsandboxed to run!")
+      return
     }
 
   function output(g) {
-     return (g)
+    return (g)
   }
   function error(g) {
     return (`Error: ${g}`)
@@ -20,56 +20,56 @@
   }
 
     class Extension {
-        getInfo() {
-          return {
-            "id": "shaman2016JavaScriptRunner",
-            "name": "JavaScript Runner",
-            "color1": "#0fbd8c",
-            "blocks": [{
-              "opcode": "block_1",
-              "text": "JS [n1], args [args]",
-              "blockType": "command",
-              "arguments": {
-                "n1": {
-                  "type": "string",
-                  "defaultValue": "function(args) { alert(\"Hello World\") }"
-                },
-                "args": {
-                  "type": "string",
-                  "defaultValue": {}
-                }
+      getInfo() {
+        return {
+          "id": "shaman2016JavaScriptRunner",
+          "name": "JavaScript Runner",
+          "color1": "#0fbd8c",
+          "blocks": [{
+            "opcode": "block_1",
+            "text": "JS [n1], args [args]",
+            "blockType": "command",
+            "arguments": {
+              "n1": {
+                "type": "string",
+                "defaultValue": "function(args) { alert(\"Hello World\") }"
+              },
+              "args": {
+                "type": "string",
+                "defaultValue": {}
               }
-            }, {
-              "opcode": "block_2",
-              "text": "JS [n2], args [args]",
-              "blockType": "reporter",
-              "arguments": {
-                "n2": {
-                  "type": "string",
-                  "defaultValue": "function() { output(\"hello\") }"
-                },
-                "args": {
-                  "type": "string",
-                  "defaultValue": {}
-                }
+            }
+          }, {
+            "opcode": "block_2",
+            "text": "JS [n2], args [args]",
+            "blockType": "reporter",
+            "arguments": {
+              "n2": {
+                "type": "string",
+                "defaultValue": "function() { output(\"hello\") }"
+              },
+              "args": {
+                "type": "string",
+                "defaultValue": {}
               }
-            }, {
-              "opcode": "block_3",
-              "text": "JS [n3], args [args]",
-              "blockType": "Boolean",
-              "arguments": {
-                "n3": {
-                  "type": "string",
-                  "defaultValue": "function(args) { output(1 < 2) }"
-                },
-                "args": {
-                  "type": "string",
-                  "defaultValue": {}
-                }
+            }
+          }, {
+            "opcode": "block_3",
+            "text": "JS [n3], args [args]",
+            "blockType": "Boolean",
+            "arguments": {
+              "n3": {
+                "type": "string",
+                "defaultValue": "function(args) { output(1 < 2) }"
+              },
+              "args": {
+                "type": "string",
+                "defaultValue": {}
               }
-            }]
-          }
+            }
+          }]
         }
+      }
       async block_1(args) {
         shaman2016JavaScriptRunner = {}
         shaman2016JavaScriptRunner.code = args["n1"]
