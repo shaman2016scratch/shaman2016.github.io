243a2f4ccc1f3ea715fda96486bdde3fc5047d32
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-17 17:20:32 +0300

    Update 1.0.js

--- extensions/extension10/1.0.js
+++ extensions/extension10/1.0.js
@@ -19,62 +19,72 @@
           color1: "#a3c0e1",
           blocks: [
             {
-              opcode: "setHead",
-              blockType: Scratch.BlockType.COMMAND,
-              text: "set headers [header]",
+              opcode: "mget",
+              blockType: Scratch.BlockType.REPORTER,
+              text: "get [url], headers: [header]",
               arguments: {
+                url: {
+                  defaultValue: "https://api.scratch.mit.edu/users/shaman2016/",
+                  type: Scratch.ArgumentType.STRING,
+                },
                 header: {
                   defaultValue: '{"Content-Type": "application/json"}',
                   type: Scratch.ArgumentType.STRING,
                 },
               },
             }, {
-              opcode: "mget",
+              opcode: "mpost",
               blockType: Scratch.BlockType.REPORTER,
-              text: "get [url]",
+              text: "post [url], headers: [header]",
               arguments: {
                 url: {
                   defaultValue: "https://api.scratch.mit.edu/users/shaman2016/",
                   type: Scratch.ArgumentType.STRING,
                 },
-              },
-            }, {
-              opcode: "mpost",
-              blockType: Scratch.BlockType.REPORTER,
-              text: "post [url]",
-              arguments: {
-                url: {
-                  defaultValue: "https://api.scratch.mit.edu/users/shaman2016/",
+                header: {
+                  defaultValue: '{"Content-Type": "application/json"}',
                   type: Scratch.ArgumentType.STRING,
                 },
               },
             }, {
               opcode: "mput",
               blockType: Scratch.BlockType.REPORTER,
-              text: "put [url]",
+              text: "put [url], headers: [header]",
               arguments: {
                 url: {
                   defaultValue: "https://api.scratch.mit.edu/users/shaman2016/",
+                  type: Scratch.ArgumentType.STRING,
+                },
+                header: {
+                  defaultValue: '{"Content-Type": "application/json"}',
                   type: Scratch.ArgumentType.STRING,
                 },
               },
             }, {
               opcode: "mdelete",
               blockType: Scratch.BlockType.REPORTER,
-              text: "delete [url]",
+              text: "delete [url], headers: [header]",
               arguments: {
                 url: {
                   defaultValue: "https://api.scratch.mit.edu/users/shaman2016/",
+                  type: Scratch.ArgumentType.STRING,
+                },
+                header: {
+                  defaultValue: '{"Content-Type": "application/json"}',
                   type: Scratch.ArgumentType.STRING,
                 },
               },
             }, {
               opcode: "mhead",
               blockType: Scratch.BlockType.REPORTER,
-              text: "head [url]",
+              text: "head [url], headers: [header]",
               arguments: {
                 url: {
                   defaultValue: "https://api.scratch.mit.edu/users/shaman2016/",
+                  type: Scratch.ArgumentType.STRING,
+                },
+                header: {
+                  defaultValue: '{"Content-Type": "application/json"}',
                   type: Scratch.ArgumentType.STRING,
                 },
               },
@@ -82,41 +92,38 @@
           ],
         };
       }
-      setHead(args) {
-        ModHttpShaman2016.headers = args['header']
-      }
       mget(args) {
         Otvet = fetch(args['url'], {  
           method: 'GET',  
-          headers: ModHttpShaman2016.headers
+          headers: args['header']
         });
         return JSON.stringify(Otvet)
       }
       mpost(args) {
         Otvet = fetch(args['url'], {  
           method: 'POST',  
-          headers: ModHttpShaman2016.headers
+          headers: args['header']
         });
         return JSON.stringify(Otvet)
       }
       mput(args) {
         Otvet = fetch(args['url'], {  
           method: 'PUT',  
-          headers: ModHttpShaman2016.headers
+          headers: args['header']
         });
         return JSON.stringify(Otvet)
       }
       mdelete(args) {
         Otvet = fetch(args['url'], {  
           method: 'DELETE',  
-          headers: ModHttpShaman2016.headers
+          headers: args['header']
         });
         return JSON.stringify(Otvet)
       }
       mhead(args) {
         Otvet = fetch(args['url'], {  
           method: 'HEAD',  
-          headers: ModHttpShaman2016.headers
+          headers: args['header']
         });
         return JSON.stringify(Otvet)
       }
